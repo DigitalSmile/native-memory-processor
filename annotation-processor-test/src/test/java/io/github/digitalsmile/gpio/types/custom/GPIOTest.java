53f1fc98e99f627587b103951bae193cda92885b
digitalsmile <digitalsmile.dig@gmail.com>
2025-03-28 09:02:01 +0300

    Fix build

--- annotation-processor-test/src/test/java/io/github/digitalsmile/gpio/types/custom/GPIOTest.java
+++ annotation-processor-test/src/test/java/io/github/digitalsmile/gpio/types/custom/GPIOTest.java
@@ -7,7 +7,7 @@ import io.github.digitalsmile.annotation.structure.Struct;
 import io.github.digitalsmile.annotation.structure.Structs;
 import io.github.digitalsmile.annotation.structure.Unions;
 
-@NativeMemory(headers = "/usr/src/linux-headers-${version}/include/uapi/linux/gpio.h")
+@NativeMemory(headers = "/usr/src/linux-headers-${linux-version}/include/uapi/linux/gpio.h")
 @NativeMemoryOptions(
         processRootConstants = true
 )
